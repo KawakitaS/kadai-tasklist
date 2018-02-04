b8cfac444feff186bca5fd17b1ea8e6571dd985c
Kawakita <kawakita.satoshi.36e@gmail.com>
2018-02-04 03:23:28 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,6 +5,8 @@
 <!-- ここにページ毎のコンテンツを書く -->
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 <p>{{ $task ->content }}</p>
+<p>ステータス: {{ $task ->status }}</p>
+
 {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
 {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
 {!! Form::submit('削除') !!}
